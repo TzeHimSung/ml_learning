02546fa193450c321b74812dd57bdc42727eef80
JHSeng <zxc98567@gmail.com>
2020-03-23 12:16:27 +0800

    update main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def main():
+    # a, b = input().split(' ')
+    # a = int(a)
+    # b = int(b)
+    # if a >= 0:
+    #     print(a+b)
+    # else:
+    #     print(a-b)
+    # print('I\'m Okay')
+    # print('''Just
+    # test
+    # huanhang
+    # ''')
+    print(not False)
+
+
+if __name__ == '__main__':
+    main()
