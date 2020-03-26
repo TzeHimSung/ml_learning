fe2dfea6e0acbecf049a4ef93d6e2508527526cf
JHSeng <zxc98567@gmail.com>
2020-03-26 17:23:34 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 
 def main():
@@ -14,8 +15,18 @@ def main():
     # test
     # huanhang
     # ''')
-    a = input()
-    a = chr(ord(a)+65-97)
+    # a = input()
+    # a = chr(ord(a)+65-97)
+    # print(a)
+    # print(len(b'ABC'))
+    # print('中文'.encode('utf-8'))
+    # a, b = input().split(' ')
+    # a = int(a)
+    # b = int(b)
+    # print('The sum of a and b is %d.' % (a + b))
+    # print('The sum of a and b is {0}'.format(a+b))
+    a = input().split(' ')
+    a = [int(a[i]) for i in range(len(a))]
     print(a)
 
 
