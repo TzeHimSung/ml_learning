6e017102eccb0ecbee088ac15e68dbe71ca742f0
JHSeng <zxc98567@gmail.com>
2020-03-31 15:49:53 +0800

    update main.py

--- main.py
+++ main.py
@@ -2,21 +2,107 @@
 # -*- coding: utf-8 -*-
 
 import math
+# from collections import Iterable
 
 
-def myAbs(number):
-    if not isinstance(number, (int, float)):
-        raise TypeError('bad operand type')
-    if number >= 0:
-        return number
-    else:
-        return -number
+# def myAbs(number):
+#     if not isinstance(number, (int, float)):
+#         raise TypeError('bad operand type')
+#     if number >= 0:
+#         return number
+#     else:
+#         return -number
 
 
-def move(x, y, step, angle=0):
-    nx = x+step*math.cos(angle)
-    ny = y-step*math.sin(angle)
-    return nx, ny
+# def move(x, y, step, angle=0):
+#     nx = x+step*math.cos(angle)
+#     ny = y-step*math.sin(angle)
+#     return nx, ny
+
+
+# def power(number, pow=2):
+#     ret = 1
+#     base = number
+#     while pow > 0:
+#         if pow & 1 != 0:
+#             ret = ret*base
+#         base = base*base
+#         pow = pow//2
+#     return ret
+
+
+# def enroll(name, age, gender='F', city='guangzhou'):
+#     if not isinstance(name, (str)):
+#         raise TypeError('Name type error')
+#     if not isinstance(age, (int)):
+#         raise TypeError('Age type error')
+#     if not isinstance(gender, (str)):
+#         raise TypeError('Gender type error')
+#     if not isinstance(city, (str)):
+#         raise TypeError('City type error')
+#     print(name, age, gender, city)
+
+
+# def add_end(a=[]):
+#     a.append('end')
+#     return a
+
+
+# def add_end(a=None):
+#     if a is None:
+#         a = []
+#     a.append('end')
+#     return a
+
+
+# def getSum(numbers):
+#     sum = 0
+#     for number in numbers:
+#         sum += number
+#     return sum
+
+# def getSum(*numbers):
+#     sum = 0
+#     for number in numbers:
+#         sum += number
+#     return sum
+
+# def personInfo(name, age, **extraArgs):
+#     print('name =', name)
+#     print('age =', age)
+#     print('other =', extraArgs)
+
+# def personInfo(name, age, *, city, job):
+#     print(name, age, city, job)
+
+
+# def personInfo(name, age, *args, city='beijing', job):
+#     print(name, age, args, city, job)
+
+
+# def niubiF(a, b, c=0, *args, d, **kw):
+#     print(a, b, c, args, d, kw)
+
+
+# def fact(number, ans=1):
+#     if not isinstance(number, (int)):
+#         raise TypeError('Number type error')
+#     if number == 1:
+#         return ans
+#     else:
+#         return fact(number-1, ans*number)
+
+
+# def check(s):
+#     if not isinstance(s, (str)):
+#         raise TypeError
+#     a = s[0:1]
+#     b = s[-1:]
+#     if a == ' ':
+#         return check(s[1:])
+#     elif b == ' ':
+#         return check(s[:-1])
+#     return s
 
 
 def main():
@@ -102,7 +188,33 @@ def main():
     # print(bool(-1))
     # a = int(input())
     # print(myAbs(a))
-    print(move(100, 100, 60, math.pi/6))
+    # print(move(100, 100, 60, math.pi/6))
+    # print(power(2, 10))
+    # print(power(2))
+    # enroll('Tom', 1, 'Boy')
+    # enroll('Jelly', 2, city='beijing')
+    # print(add_end())
+    # print(add_end())
+    # print(add_end())
+    # print(getSum(1, 2, 3, 4))
+    # a = (2, 3, 4, 5)
+    # print(getSum(*a))
+    # personInfo('Tom', 18, city='guangzhou', job='programmer')
+    # personInfo('Tom', 18, 1, 2, 3, job='programmer', other='test')
+    # niubiF(1, 2, 0, 1, 2, 3, k=1, d=1)
+    # print(fact(1000))
+    # a = list(range(10))
+    # a.reverse()
+    # print(a[-8:-5:2])
+    # print(check('    12313 123132  '))
+    # ddict = {'a': 1, 'c': 3, 'b': 2}
+    # for key, val in ddict.items():
+    #     print(key, val)
+    # print(isinstance(ddict, (Iterable)))
+    # a = list(range(10))
+    # for key, val in enumerate(a, 1):
+    #     print(key, val)
+    print([x*x for x in range(1, 11) if x & 1 == 1])
 
 
 if __name__ == '__main__':
