77e5feac3662044936ad750ab6ee3580398caefd
TzeHimSung <zxc98567@163.com>
2020-04-07 10:28:17 +0800

    update main.py

--- main.py
+++ main.py
@@ -649,6 +649,21 @@ def main():
 
     # print(f(1, 2, 3))
 
+    # class Solution:
+    #     def rotate(self, matrix: List[List[int]]) -> None:
+    #         """
+    #         Do not return anything, modify matrix in-place instead.
+    #         """
+    #         n = len(matrix)
+    #         matrix2 = []
+    #         for i in range(n):
+    #             matrix2.append([])
+    #         for i in range(n-1, -1, -1):
+    #             for j in range(n):
+    #                 matrix2[j].append(matrix[i][j])
+
+    #         matrix[:] = matrix2[:]
+
 
 if __name__ == '__main__':
     main()
