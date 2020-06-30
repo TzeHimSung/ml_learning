9a81c1ec80add2e518020b0e3039c336972fa800
JHSeng <zxc98567@gmail.com>
2020-07-01 00:44:18 +0800

    update bk frontend class

---
+++ frontend_learning/class/main.js
@@ -0,0 +1,65 @@
+// var f = function (a, b) {
+//     console.log(a + b)
+// }
+
+// function f2(a, b) {
+//     console.log(a, b)
+// }
+
+// var arr = [1, 2, 3, 4]
+// arr.sort((a, b) => {
+//     return b - a
+// })
+
+// console.log(arr)
+
+// var name = 'zhangsan'
+// if (true) {
+//     var name = 'lisi'
+// }
+
+// function f() {
+//     // var a = '123'
+//     b = '456'
+// }
+
+// console.log(a)
+// console.log(b)
+// var b = 123
+// console.log(b)
+
+// console.log(a)
+// console.log(b)
+// function a() {
+// }
+// var b = function () {
+// }
+
+// 1. 寻找变量声明和参数
+// 2. 实参形参统一
+// 3. 函数
+
+// function fn(a) {
+//     console.log(a)
+//     var a = 123
+//     console.log(a)
+//     function a() { }
+//     console.log(a)
+//     var b = function () { }
+//     console.log(b)
+//     function d() {
+//         console.log(a)
+//     }
+//     d()
+// }
+
+// function a() {
+//     var n = 100
+//     function b() {
+//         console.log(n)
+//     }
+//     return b
+// }
+
+// a()()
+
